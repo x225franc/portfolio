61edcd2e1bd6c859742aba84664c31589abc2a2e
Alpha Malick DIAWARA <102668503+x225franc@users.noreply.github.com>
2024-10-16 11:57:43 +0200

    update

--- app/page.js
+++ app/page.js
@@ -191,7 +191,7 @@ export default function Home() {
 										<br />
 									</p>
 									<div>
-										<a href='uploads/CV_dl.pdf' target='_blank'>
+										<a href='uploads/CV.pdf' target='_blank'>
 											<img
 												src='uploads/CV.png'
 												alt=''
